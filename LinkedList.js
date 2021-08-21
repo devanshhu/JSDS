c60a4173ae9576148fbe7081f34d0d1520ba9851
Devansh Upadhyay <devanshu9719@gmail.com>
2021-08-21 23:09:59 +0530

    LinkedList- Added Object handling & documentation

--- LinkedList.js
+++ LinkedList.js
@@ -1,9 +1,17 @@
 // -----------------------------------------Linked list starts-------------------------------------------
 class LinkedList {
-    constructor() {
-        this.head = null;
+    /**
+     * @constructor
+     * Create a linked-list based on given seed value. default value of head is null.
+     */
+    constructor(seed_head = null) {
+        this.head = seed_head;
     }
 
+    /**
+     * Append element at the end of the list.
+     * @param {any} value add param to linked-list.
+     */
     append(value) {
         const newObject = { value, next: null };
         if (!this.head) {
@@ -17,13 +25,21 @@ class LinkedList {
         item.next = newObject;
     }
 
-    addToFront(value) {
+    /**
+     * Add element to the first position in linked-list.
+     * @param {*} value 
+     */
+    addToStart(value) {
         const obj = { value, next: this.head };
         this.head = obj;
 
     }
 
-    deleteFromFront() {
+    /**
+     * Deletes the first element of the linked-list. Throws error in case of empty linked-list.
+     * @returns {any} Deleted element
+     */
+    deleteFromStart() {
         if (!this.head) {
             throw Error("List empty !!!!");
         }
@@ -34,7 +50,11 @@ class LinkedList {
         }
     }
 
-    deleteFromRear() {
+    /**
+     * Deletes the last element of the linked-list. Throws error in case of empty linked-list.
+     * @returns { any } Deleted element.
+     */
+    deleteFromEnd() {
 
         if (!this.head) {
             throw Error("List empty!!!!");
@@ -50,28 +70,72 @@ class LinkedList {
         item.next = null;
     }
 
-    print() {
+    /**
+     * 
+     * @param {Function} callback Function to use for mapping of every object in linked-list.
+     */
+    print(callback = (x) => x) {
+        if (!this.head)
+            return [];
         let result = [];
         let item = this.head;
         while (item) {
             result.push(item.value);
             item = item.next;
         }
-        console.log(result);
+        console.log(result.map(callback));
+    }
+
+    /**
+     * 
+     * @param {Function} callback Function to use for mapping of every object in linked-list.
+     * @returns { Array<any> } array of mapped elements
+     */
+    getLinkedList(callback = (x) => x) {
+        if (!this.head)
+            return [];
+        let result = [];
+        let item = this.head;
+        while (item) {
+            result.push(item.value);
+            item = item.next;
+        }
+        return result.map(callback);
+    }
+
+    /**
+     * Returns the size of linked-list.
+     * @returns {number} size
+     */
+    size() {
+        return this.item.length;
+    }
+
+    findElement(item, key = (x) => x) {
+
+
+
     }
 }
 
 const ll1 = new LinkedList();
-ll1.append(1);
-ll1.append(2);
-ll1.append(3);
-ll1.append(4);
-ll1.addToFront(0);
-ll1.deleteFromFront();
-ll1.deleteFromRear();
-ll1.deleteFromRear();
-ll1.deleteFromRear();
-ll1.deleteFromRear();
-ll1.print();
+ll1.append({
+    id: 1,
+    name: "A"
+});
+ll1.append({
+    id: 2,
+    name: "B"
+});
+ll1.append({
+    id: 3,
+    name: "C"
+});
+// ll1.deleteFromEnd();
+// ll1.deleteFromEnd();
+// ll1.deleteFromEnd();
+// ll1.deleteFromEnd();
+ll1.print(x => x.id);
+console.log(ll1.getLinkedList(x => x.name))
 
 //-------------------Linked list ends---------------------------------------------------
